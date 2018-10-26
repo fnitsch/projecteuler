fbf38d6d860ca6557587a95fa725513540f53099
Fabian Nitsch <f.nitsch@outlook.com>
2018-10-26 03:27:30 +0200

    Returns the largest prime factor of the number 600851475143.

---
+++ problem_3/src/com/example/euler/problem_3/Problem3.java
@@ -0,0 +1,34 @@
+package com.example.euler.problem_3;
+
+import java.math.BigInteger;
+
+/**
+ * Created by Fabian.Nitsch on 1/20/2018
+ */
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+        long n = 6008514751430L;
+
+        primeFactorization(n);
+
+    }
+
+    private static void primeFactorization(long n) {
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0 && n != i) {
+                System.out.println(i);
+                primeFactorization(n / i);
+                return;
+
+            } else if (n % i == 0 && n == i){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
+}
